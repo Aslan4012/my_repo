7f194e917988106a84e80cab3ede1f72f19a9d68
Aslan4012 <aslan11@icloud.com>
2026-01-14 01:10:49 +0100

    train changed

--- src/my_project/train.py
+++ src/my_project/train.py
@@ -1,33 +1,33 @@
-
 import matplotlib.pyplot as plt
 import torch
 import pytorch_lightning as pt
 from pytorch_lightning.callbacks import ModelCheckpoint
 import typer
-from src.my_project.data import corrupt_mnist
-from src.my_project.model import MyAwesomeModel
+from data import corrupt_mnist
+from model import MyAwesomeModel
 
-def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+
+def train(lr: float = 1e-3, batch_size: int = 128, epochs: int = 10) -> None:
     """Train a model on MNIST."""
     print("Training day and night")
     print(f"{lr=}, {batch_size=}, {epochs=}")
     # Data
     train_set, _ = corrupt_mnist()
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
-    
+
     # Model and trainer
     model = MyAwesomeModel()
-    checkpoint_callback = ModelCheckpoint(
-    dirpath="models/",          # where to save
-    filename="model-{epoch}",   # file name pattern
-    save_top_k=1,               # keep best model
-    monitor="train_loss",       # or val_loss if you have validation
-    mode="min",
-    )
-    
+    # checkpoint_callback = ModelCheckpoint(
+    #     dirpath="models/",  # where to save
+    #     filename="model-{epoch}",  # file name pattern
+    #     save_top_k=1,  # keep best model
+    #     monitor="train_loss",  # or val_loss if you have validation
+    #     mode="min",
+    # )
+
     trainer = pt.Trainer(
         max_epochs=epochs,
-        accelerator="auto",   # uses CUDA/MPS/CPU automatically
+        accelerator="auto",  # uses CUDA/MPS/CPU automatically
         devices="auto",
         log_every_n_steps=50,
     )
@@ -38,6 +38,7 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
 
 if __name__ == "__main__":
     train()
+
 
 def main() -> None:
     train()
